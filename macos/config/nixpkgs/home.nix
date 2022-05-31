7407b48f82f69d4bd3682e24d76ff784b215448f
Vitaly Lavrov <lavrovvv@gmail.com>
2022-05-31 13:36:23 +0200

    Add macos nix conifgs

---
+++ macos/config/nixpkgs/home.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "vitaly";
+  home.homeDirectory = "/Users/vitaly";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Packages that should be installed to the user profile.
+  home.packages = [
+    pkgs.htop
+    pkgs.jetbrains.idea-community
+    pkgs.coursier
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Vitaly Lavrov";
+    userEmail = "lavrovvv@gmail.com";
+  };
+
+  programs.neovim = {
+    enable = true;
+  };
+
+  # programs.firefox = {
+  #   enable = true;
+  # };
+
+  programs.password-store = {
+    enable = true;
+  };
+
+  programs.java = {
+    enable = true;
+  };
+
+  programs.vscode = {
+    enable = true;
+  };
+
+  programs.gpg = {
+    enable = true;
+    scdaemonSettings = {
+      disable-ccid = true;
+    };
+  };
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+      gpgconf --launch gpg-agent
+    '';
+  };
+}
