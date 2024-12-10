dd8a92ebe0ffa163d980d1d8f7dc1ad94b4d914c
Vitaly Lavrov <lavrovvv@gmail.com>
2024-12-10 23:59:28 +0100

    Add direnv

--- home/common.nix
+++ home/common.nix
@@ -1,71 +1,79 @@
 { config, pkgs, ... }:
 
 {
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-    vimdiffAlias = true;
-    plugins = with pkgs.vimPlugins; [
-      unison
-    ];
-    coc = {
+  programs = {
+    neovim = {
       enable = true;
-      settings = {
-        languageserver = {
-	  unison = {
-	    filetypes = ["unison"];
-	    host = "127.0.0.1";
-	    port = 5757;
-	    settings = {};
-	  };
-	};
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        unison
+      ];
+      coc = {
+        enable = true;
+        settings = {
+          languageserver = {
+              unison = {
+                filetypes = ["unison"];
+                host = "127.0.0.1";
+                port = 5757;
+                settings = {};
+              };
+  	      };
+        };
+      };
+      extraLuaConfig = ''
+        -- Normal mode -> command mode re-mapping for semicolon (;)
+        vim.keymap.set('n', ';', ':', {})
+      '';
+    };
+
+    password-store = {
+      enable = true;
+      package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
+    };
+
+    gpg = {
+      enable = true;
+      scdaemonSettings = {
+        disable-ccid = true;
       };
     };
-    extraLuaConfig = ''
-      -- Normal mode -> command mode re-mapping for semicolon (;)
-      vim.keymap.set('n', ';', ':', {})
-    '';
-  };
 
-  programs.password-store = {
-    enable = true;
-    package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
-  };
-
-  programs.gpg = {
-    enable = true;
-    scdaemonSettings = {
-      disable-ccid = true;
-    };
-  };
-
-  services.gpg-agent = {
-    enable = true;
-    enableSshSupport = true;
-  };
-
-  programs.bat = {
-    enable = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-    autosuggestion = {
+    bat = {
       enable = true;
     };
 
-    enableCompletion = true;
-
-    oh-my-zsh = {
+    zsh = {
       enable = true;
-      theme = "refined";
-      plugins=[
-       "git"
-       "vi-mode"
-       "z"
-      ];
+      autosuggestion = {
+        enable = true;
+      };
+
+      enableCompletion = true;
+
+      oh-my-zsh = {
+        enable = true;
+        theme = "refined";
+        plugins=[
+         "git"
+         "vi-mode"
+         "z"
+        ];
+      };
+    };
+
+    direnv = {
+      enable = true;
+    };
+  };
+
+  services = {
+    gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
     };
   };
 }
