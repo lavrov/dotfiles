595244b83d444e496a8815135431b2879d51effc
Vitaly Lavrov <vitaly.lavrov@coralogix.com>
2026-03-11 11:48:29 +0100

    Update darwin-common.nix

--- home/darwin-common.nix
+++ home/darwin-common.nix
@@ -6,6 +6,8 @@
     enableZshIntegration = true;
     settings = {
       font-size = 14;
+      window-width = 202;
+      window-height = 56;
       macos-titlebar-style = "tabs";
       theme = "GitHub Light Default";
     };
