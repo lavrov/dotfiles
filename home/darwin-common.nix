6e89469f9d6ccd1b115b5a3340681c5f5224487f
Vitaly Lavrov <vitaly.lavrov@coralogix.com>
2026-03-11 11:35:41 +0100

    Remove font-family from Ghostty config - use system default

--- home/darwin-common.nix
+++ home/darwin-common.nix
@@ -5,8 +5,7 @@
     package = pkgs.ghostty-bin;
     enableZshIntegration = true;
     settings = {
-      font-size = 16;
-      font-family = "JetBrainsMono";
+      font-size = 14;
       macos-titlebar-style = "tabs";
       theme = "GitHub Light Default";
     };
