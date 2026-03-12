1f1d2cbdd7c5139425b6b9b8be53cd61f521fe1b
Vitaly Lavrov <vitaly.lavrov@coralogix.com>
2026-03-12 12:11:12 +0100

    Adjust ghostty font thickening strength to 40

--- home/darwin-common.nix
+++ home/darwin-common.nix
@@ -6,6 +6,8 @@
     enableZshIntegration = true;
     settings = {
       font-size = 14;
+      font-thicken = true;
+      font-thicken-strength = 40;
       window-width = 202;
       window-height = 56;
       macos-titlebar-style = "tabs";
