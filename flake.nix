7b1c850c59450fffc577b7cbc395250b02ce2c1d
Vitaly Lavrov <vitaly.lavrov@coralogix.com>
2026-08-05 15:21:12 +0200

    Install opencode-desktop via opencode flake overlay

--- flake.nix
+++ flake.nix
@@ -11,23 +11,35 @@
       url = "github:coralogix/protofetch";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    opencode = {
+      url = "github:anomalyco/opencode";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, protofetch, ... }: {
+  outputs = { nixpkgs, home-manager, protofetch, opencode, ... }: {
     homeConfigurations = {
       "vitaly@framework-13" = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
         modules = [ ./home/framework-13.nix ];
       };
       "coralogix" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { system = "aarch64-darwin"; config.allowUnfree = true; };
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config.allowUnfree = true;
+          overlays = [ opencode.overlays.default ];
+        };
         modules = [ ./home/coralogix-macbook-pro.nix ];
         extraSpecialArgs = {
           inherit protofetch;
         };
       };
       "vitaly" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { system = "aarch64-darwin"; config.allowUnfree = true; };
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config.allowUnfree = true;
+          overlays = [ opencode.overlays.default ];
+        };
         modules = [ ./home/vitaly-macbook-pro.nix ];
       };
     };
