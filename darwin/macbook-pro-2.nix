85465e52bc0a7bcf9cc6125b87d0c94df2dfe438
Vitaly Lavrov <vitaly.lavrov@coralogix.com>
2026-08-24 13:43:30 +0200

    darwin/macbook-pro-2: authorize YubiKey ed25519 key for vitaly.lavrov
    Add the SSH public key from the GPG agent (cardno:15_499_727) to
    users.users."vitaly.lavrov".openssh.authorizedKeys.keys so sshd
    accepts key-based login from the primary laptop.

--- darwin/macbook-pro-2.nix
+++ darwin/macbook-pro-2.nix
@@ -5,6 +5,10 @@
 
   networking.localHostName = "macbook-pro-2";
 
+  users.users."vitaly.lavrov".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFlCf6iiHJrtIWLplXwJuI+ZRPcrhqR+rWT7+eOcRtRc"
+  ];
+
   services.openssh = {
     enable = true;
     extraConfig = ''
